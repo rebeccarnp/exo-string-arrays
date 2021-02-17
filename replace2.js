f504a8450ded2f89b0379dfb92374d42eafa815d
rebeccarnp <rebecca.remacle@gmail.com>
2021-02-17 23:01:26 +0100

    replace 2

---
+++ replace2.js
@@ -0,0 +1,5 @@
+let text = 'In cryptography, Alice and Bob are fictional characters commonly used as placeholders in discussions about cryptographic protocols or systems.\n\
+As the use of Alice and Bob became more widespread, additional characters were added, sometimes each with a particular meaning.\n\
+The most common characters are Alice and Bob. Eve, Mallory, and Trent are also common names.';
+
+console.log(text.replaceAll('Alice', 'Rebecca'));
